dfa5605535b28699d7e72330bbefb016bf2914ec
Crowdbotics <team@crowdbotics.com>
2023-06-06 17:25:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-bird-41301",
+  url: "https://lucky-bird-41301.botics.co",
+  api: "https://lucky-bird-41301.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
